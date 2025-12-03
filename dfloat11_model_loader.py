e90196a16f04032b83e921ffb8b9e7f551d7382e
mingyi456 <mingyi456@hotmail.com>
2025-12-03 12:23:33 +0800

    Fix bug caused by last commit

--- dfloat11_model_loader.py
+++ dfloat11_model_loader.py
@@ -103,69 +103,7 @@ class DFloat11ModelLoader(DFloat11ModelLoaderAdvanced):
     FUNCTION = "load_dfloat11_model"
     
     def load_dfloat11_model(self, dfloat11_model_name):
-        return self.load_dfloat11_model_advanced(dfloat11_model_name, cpu_offload = False, cpu_offload_blocks = 0, pin_memory = True)
-
-'''
-class DFloat11ModelLoader(DFloat11ModelLoaderAdvanced):
-    """
-    A custom node to load a DFloat11 diffusion model from the `diffusion_models` directory.
-
-    DFloat11 models are >30% smaller than their float16 counterparts, yet produce bit-for-bit identical outputs.
-    """
-
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "dfloat11_model_name": (folder_paths.get_filename_list("diffusion_models"),),
-            }
-        }
-
-    RETURN_TYPES = ("MODEL",)
-    FUNCTION = "load_dfloat11_model"
-    CATEGORY = "DFloat11"
-
-    def load_dfloat11_model(self, dfloat11_model_name):
-        dfloat11_model_path = folder_paths.get_full_path_or_raise("diffusion_models", dfloat11_model_name)
-        state_dict = comfy.utils.load_torch_file(dfloat11_model_path)
-
-        if not any(k.endswith("encoded_exponent") for k in state_dict.keys()):
-            raise ValueError(f"The model '{dfloat11_model_name}' is not a DFloat11 model.")
-
-        load_device = comfy.model_management.get_torch_device()
-        offload_device = comfy.model_management.unet_offload_device()
-
-        model_config = comfy.sd.model_detection.model_config_from_unet(state_dict, "")
-        
-        if model_config is None:
-            # Assume it is CosmosPredict2, because no other model architectures are supported yet
-            state_dict["blocks.0.mlp.layer1.weight"] = None
-            model_config = comfy.sd.model_detection.model_config_from_unet(state_dict, "")
-            assert model_config is not None, "Unable to detect model type"
-        
-        df11_type = type(model_config).__name__
-        
-        model_config.set_inference_dtype(torch.bfloat16, torch.bfloat16)
-        model = model_config.get_model(state_dict, "")
-        model = model.to(offload_device)
-
-        DFloat11Model.from_single_file(
-            dfloat11_model_path,
-            pattern_dict=MODEL_TO_PATTERN_DICT[df11_type],
-            bfloat16_model=model.diffusion_model,
-            device=offload_device,
-        )
-        
-
-        if df11_type == "Chroma":
-            return (
-                CustomChromaModelPatcher(model, load_device=load_device, offload_device=offload_device),
-            )
-        return (
-            comfy.model_patcher.ModelPatcher(model, load_device=load_device, offload_device=offload_device),
-        )
-'''
-
+        return self.load_dfloat11_model_advanced(dfloat11_model_name, cpu_offload = False, cpu_offload_blocks = 0, pin_memory = True, custom_modelpatcher = True)
 
 class CheckpointLoaderWithDFloat11(CheckpointLoaderSimple):
     @classmethod
